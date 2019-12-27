f7e993c1b6433162d72f082b27ec6a845e478aa1
Matt Stillwell <stillwell006@gmail.com>
2019-12-27 17:26:04 -0500

    Made borders lighter

--- src/index.tsx
+++ src/index.tsx
@@ -3,91 +3,19 @@ import * as React from 'react'
 import { render } from 'react-dom'
 
 // Import interfaces
-import { TodoInterface, BubbleInterface } from './interfaces'
-
+import { BubbleInterface } from './interfaces'
 
 // Import components
-import TodoForm from './components/todo-form'
-import TodoList from './components/todo-list'
 import BubbleList from './components/bubble-list'
-import NavBar from './components/navbar'
+import Topbar from './components/topbar';
 
 // Import styles
-// import './styles/styles.scss'
-// import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './magic/hidesidebar.js'
+
 import './styles/sidebar.scss'
 import './styles/styles.scss'
-
-// TodoListApp component
-const TodoListApp = () => {
-  const [todos, setTodos] = React.useState<TodoInterface[]>([])
-
-  // Creating new todo item
-  function handleTodoCreate(todo: TodoInterface) {
-    // Prepare new todos state
-    const newTodosState: TodoInterface[] = [...todos]
-    // Update new todos state
-    newTodosState.push(todo)
-    // Update todos state
-    setTodos(newTodosState)
-  }
-
-  // Update existing todo item
-  function handleTodoUpdate(event: React.ChangeEvent<HTMLInputElement>, id: string) {
-    // Prepare new todos state
-    const newTodosState: TodoInterface[] = [...todos]
-    // Find correct todo item to update
-    newTodosState.find((todo: TodoInterface) => todo.id === id)!.text = event.target.value
-    // Update todos state
-    setTodos(newTodosState)
-  }
-
-  // Remove existing todo item
-  function handleTodoRemove(id: string) {
-    // Prepare new todos state
-    const newTodosState: TodoInterface[] = todos.filter((todo: TodoInterface) => todo.id !== id)
-    // Update todos state
-    setTodos(newTodosState)
-  }
-
-  // Check existing todo item as completed
-  function handleTodoComplete(id: string) {
-    // Copy current todos state
-    const newTodosState: TodoInterface[] = [...todos]
-    // Find the correct todo item and update its 'isCompleted' key
-    newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted = !newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted
-    // Update todos state
-    setTodos(newTodosState)
-  }
-
-  // Check if todo item has title
-  function handleTodoBlur(event: React.ChangeEvent<HTMLInputElement>) {
-    if (event.target.value.length === 0) {
-      event.target.classList.add('todo-input-error')
-    } else {
-      event.target.classList.remove('todo-input-error')
-    }
-  }
-
-  return (
-    <div className="todo-list-app">
-      <TodoForm
-        todos={todos}
-        handleTodoCreate={handleTodoCreate}
-      />
-      <TodoList
-        todos={todos}
-        handleTodoUpdate={handleTodoUpdate}
-        handleTodoRemove={handleTodoRemove}
-        handleTodoComplete={handleTodoComplete}
-        handleTodoBlur={handleTodoBlur}
-      />
-    </div>
-  )
-}
 
 
 const bubbles: Array<BubbleInterface> = [
@@ -111,16 +39,11 @@ const bubbles: Array<BubbleInterface> = [
 const App = () => {
   return (
     <div>
-      {/* <NavBar 
-        username=""
-        points={3}
-        avatar="unknown.jpg"
-      /> */}
-      <div className="container content">
+      <Topbar />
+      <div className="container-fluid content">
         <BubbleList
           bubbles={bubbles}
         />
-        {/* <TodoListApp /> */}
       </div>
     </div>
   )
